44443c2397ab52500517c8a00b683450650c81ef
Diak Uddin Maulidi <d14k.010@gmail.com>
2024-07-08 00:10:40 +0800

    first commit from this project

---
+++ index.js
@@ -0,0 +1,23 @@
+let bgNavbar = document.getElementById("bgNavbar");
+let colorText = document.querySelectorAll("ul li a");
+colorText.forEach((item) => {
+  item.style.color = "white";
+});
+
+window.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop > 100) {
+    bgNavbar.style.backgroundColor = "white";
+    bgNavbar.style.boxShadow = "1px 1px 8px black";
+    bgNavbar.style.transition = ".5s";
+    colorText.forEach((item) => {
+      item.style.color = "black";
+    });
+  } else {
+    bgNavbar.style.backgroundColor = "";
+    bgNavbar.style.boxShadow = "";
+    bgNavbar.style.transition = ".5s";
+    colorText.forEach((item) => {
+      item.style.color = "white";
+    });
+  }
+});

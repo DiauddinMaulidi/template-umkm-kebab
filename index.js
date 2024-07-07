317677e59f644d7d54815e9a8c5a4005d453b801
Diak Uddin Maulidi <d14k.010@gmail.com>
2024-07-08 01:00:34 +0800

    modifikasi

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let bgNavbar = document.getElementById("bgNavbar");
 let colorText = document.querySelectorAll("ul li a");
-colorText.forEach((item) => {
-  item.style.color = "white";
-});
+// colorText.forEach((item) => {
+//   item.style.color = "#ffdb00";
+// });
 
 window.addEventListener("scroll", function () {
   if (document.documentElement.scrollTop > 100) {
@@ -10,14 +10,14 @@ window.addEventListener("scroll", function () {
     bgNavbar.style.boxShadow = "1px 1px 8px black";
     bgNavbar.style.transition = ".5s";
     colorText.forEach((item) => {
-      item.style.color = "black";
+      item.style.color = "#ffdb00";
     });
   } else {
     bgNavbar.style.backgroundColor = "";
     bgNavbar.style.boxShadow = "";
     bgNavbar.style.transition = ".5s";
     colorText.forEach((item) => {
-      item.style.color = "white";
+      item.style.color = "";
     });
   }
 });
